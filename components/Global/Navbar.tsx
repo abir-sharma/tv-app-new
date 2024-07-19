acc97ab4c649b783dbe6c0d6822b5cd214a48909
Arnab Roy <mr.roy.arnab@gmail.com>
2024-07-19 10:22:33 +0530

    chore: Update app version to 1.0.1 and add phone dropdown in Navbar

--- components/Global/Navbar.tsx
+++ components/Global/Navbar.tsx
@@ -20,6 +20,8 @@ import sendMongoAnalytics from "../../utils/sendMongoAnalytics";
 export default function Navbar() {
   const { isOnline, setLogs, setIsOnline, headers, setHeaders } = useGlobalContext();
   const navigation = useNavigation();
+  const [phone, setPhone] = useState<string|null>(null);
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
   const handleLogout = async () => {
     try {
@@ -41,8 +43,11 @@ export default function Navbar() {
     }
   };
 
-  const [phone, setPhone] = useState<string|null>(null);
-  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const Seperator = () => {
+    return (
+      <View className="w-full h-[1px] bg-white/40"></View>
+    )
+  }
 
   useEffect(()=>{
     const getPhone = async () => {
@@ -63,8 +68,10 @@ export default function Navbar() {
           <TouchableWithoutFeedback onPress={() => setIsDropdownVisible(false)}>
             <View style={{ flex: 1 }}>
               <ScrollView className='bg-[#111111]/90 border-white/20 border-[1px] max-h-[200] overflow-hidden w-[10%] rounded-lg absolute top-[70] right-[20] z-[2]'>
-                <Pressable onPress={()=>{}} className="w-full px-5 py-2"><Text className="text-white text-sm font-bold">{phone || "---"}</Text></Pressable>
-                <View className="w-full h-[1px] bg-white/40"></View>
+                <View className="w-full px-5 py-2"><Text className="text-white text-sm font-bold">v1.0.1</Text></View>
+                <Seperator />
+                <View className="w-full px-5 py-2"><Text className="text-white text-sm font-bold">{phone || "---"}</Text></View>
+                <Seperator />
                 <Pressable onPress={handleLogout} className="w-full px-5 py-2 rounded-b-lg"><Text className="text-white font-bold text-sm">Logout</Text></Pressable>
               </ScrollView>
             </View>
