3dcc2384313bf790c1cf8a59685e7e1ee3587cf3
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-08-23 12:37:37 +0530

    v1.0.6

--- utils/sendGoogleAnalytics.ts
+++ utils/sendGoogleAnalytics.ts
@@ -23,7 +23,7 @@ const sendGoogleAnalytics = async (eventName: string, data: EventData) => {
   }
 
   if (!data.app_version) {
-    const appVersion = '1.0.5';
+    const appVersion = '1.0.6';
     data.app_version = appVersion;
   }
 
