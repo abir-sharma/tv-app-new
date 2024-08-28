921518598825a9c6023b424046e12297bb021f2a
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-08-28 11:21:44 +0530

    test1

--- App.js
+++ App.js
@@ -66,7 +66,7 @@ export default function App() {
         await Updates.reloadAsync();
         Sentry.captureMessage('Successfully Updated');
       }
-  
+       
 
         // Alert.alert(
         //   "Update Available",
